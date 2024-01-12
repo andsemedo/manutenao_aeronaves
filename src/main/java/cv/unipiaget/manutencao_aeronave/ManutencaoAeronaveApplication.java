0b1ec7327ff6430f77680285c47ffcdfdcfade97
Anderson Semedo <andsemedo023@gmail.com>
2024-01-12 16:30:37 -0100

    commit inicial

---
+++ src/main/java/cv/unipiaget/manutencao_aeronave/ManutencaoAeronaveApplication.java
@@ -0,0 +1,13 @@
+package cv.unipiaget.manutencao_aeronave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoAeronaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoAeronaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cv/unipiaget/manutencao_aeronave/ManutencaoAeronaveApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.unipiaget.manutencao_aeronave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoAeronaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
