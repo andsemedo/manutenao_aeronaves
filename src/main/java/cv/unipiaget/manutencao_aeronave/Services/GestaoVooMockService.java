18f7ee6ddde177dbf4a7c7fc0a1c04939933bd5d
Anderson Semedo <andsemedo023@gmail.com>
2024-01-14 03:31:57 -0100

    criacao do mock service do subsistema de gestao de voo

---
+++ src/main/java/cv/unipiaget/manutencao_aeronave/Services/GestaoVooMockService.java
@@ -0,0 +1,32 @@
+package cv.unipiaget.manutencao_aeronave.Services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Anderson Semedo
+ * @Date 14/01/2024
+ */
+
+@Service
+public class GestaoVooMockService {
+
+    public String verificarDisponibilidadeAeronave(int idAeronave) {
+        if(idAeronave == 1 || idAeronave == 4 || idAeronave == 5) {
+            return "sim";
+        }
+        return "não";
+    }
+
+    public String obterRequisitosManutencaoAeronave(int idAeronave) {
+        if (idAeronave == 1) {
+            return "Check engine, Troca de óleo";
+        } else if (idAeronave == 2) {
+            return "Substituição de filtros, Inspeção estrutural";
+        } else if (idAeronave == 5) {
+            return "Verificação de sistemas, Limpeza do motor";
+        } else {
+            return null;
+        }
+    }
+
+}
